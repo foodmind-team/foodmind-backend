44b671fc611ec682616a237931ac1ccb46465c70
chenyaqi <terrence.yaqi.chen@u.nus.edu>
2026-07-27 22:44:14 +0800

    Init

---
+++ src/main/java/com/foodmind/foodmindbackend/FoodmindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodmind.foodmindbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmindBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodmindBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodmind/foodmindbackend/FoodmindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmind.foodmindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmindBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
